6a78f8a50e014d818a2e3e88733fee1259ccecd6
TharHtet236401 <tharhteta3@gmail.com>
2025-04-23 11:35:36 +0700

    Add Transaction model with fields for user, amount, category, description, and date

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Transaction(models.Model):
+    CATEGORY_CHOICES = [
+        ('Food', 'Food'),
+        ('Transportation', 'Transportation'),
+        ('Entertainment', 'Entertainment'),
+        ('Other', 'Other'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES)
+    description = models.TextField()
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.description} - {self.amount} - {self.category} - {self.date}"
