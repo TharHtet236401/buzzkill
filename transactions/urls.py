5ec1f4ccc8c33ff4738d8d7eb57f51a966b99dfb
TharHtet236401 <tharhteta3@gmail.com>
2025-04-23 12:06:58 +0700

    Refactor navigation and URL routing: update template to load static files, adjust logo size, and change navigation links to use Django URL routing for Transactions, Budget, Reports, and Settings.

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.transactions, name='transactions'),
+    path('budget/', views.budget, name='budget'),
+    path('reports/', views.reports, name='reports'),
+    path('settings/', views.settings, name='settings'),
 ]
 
--- transactions/views.py
+++ transactions/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index(request):
+def transactions(request):
     return render(request, 'transactions/transactions.html')
 
+def budget(request):
+    return render(request, 'transactions/budget.html')
+
+def reports(request):
+    return render(request, 'transactions/reports.html')
+
+def settings(request):
+    return render(request, 'transactions/settings.html')
