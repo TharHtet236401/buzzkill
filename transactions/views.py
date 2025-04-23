4d84b903a3bfc4dddf59114f8e8a9210c24bb4b0
TharHtet236401 <tharhteta3@gmail.com>
2025-04-23 13:23:56 +0700

    Refactor transactions view to implement pagination for transaction listings, enhancing performance and user experience. Adjust rendering logic for both full and partial views based on HX-Request header.

--- transactions/views.py
+++ transactions/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from .models import Transaction
+from django.core.paginator import Paginator
 # Create your views here.
 def transactions(request):
+    if request.headers.get('HX-Request'):
+        transactions = Transaction.objects.all()
+        paginator = Paginator(transactions, 10)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return render(request, 'transactions/partials/transaction-table.html', {'page_obj': page_obj})
     transactions = Transaction.objects.all()
-    return render(request, 'transactions/transactions.html', {'transactions': transactions})
+    paginator = Paginator(transactions, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'transactions/transactions.html', {'page_obj': page_obj})
 
 def budget(request):
     return render(request, 'transactions/budget.html')
